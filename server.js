42c00df79be8e29d9b47b7af710db7574025314a
jyotirsai <jyotirsai28@gmail.com>
2020-09-05 21:32:20 -0600

    added ejs

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.render("view");
 });
 
 app.listen(5000, function () {
